b5f947d5f73347b185ab52c8477f3ef2aca39f90
btvoidx <btvoidx@gmail.com>
2020-12-17 20:49:15 +0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ app = Sanic(name='api')
 @app.route('/api/callback', methods=['POST'])
 async def callback(request):
 	if request.json.get('type') == 'confirmation':
-		return text(environ('confirmation_token'))
+		return text(environ.get('confirmation_token', ''))
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=8000, debug=True)
