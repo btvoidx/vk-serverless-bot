3a5f48fd6864ee1df389f82afda17c1fa64505c6
btvoidx <btvoidx@gmail.com>
2020-12-17 21:11:21 +0400

    simple command

--- api/index.py
+++ api/index.py
@@ -6,13 +6,18 @@ app = Sanic(name='api')
 
 @app.route('/api/callback', methods=['POST'])
 async def callback(request):
-	if environ.get('secret') != request.json.get('secret'):
+	if environ.get('secret', '') != request.json.get('secret', ''):
 		return text('invalid secret')
 
 	if request.json.get('type') == 'confirmation':
 		return text(environ.get('confirmation_token', ''))
 	
-	print(request.json)
+	elif request.json.get('type') == 'message_new':
+		msg = request.json['object']['text']
+		from_id = request.json['object']['from_id']
+
+		if msg:
+			requests.get(f"https://api.vk.com/method/messages.send?message={msg}&user_id={from_id}&access_token={environ.get('access_token', '')}&v=5.78").json()
 
 	return text('ok')
 
