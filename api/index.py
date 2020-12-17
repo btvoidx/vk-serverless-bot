798f3e92da83e6f872c3a88ab813539f2c255e7a
btvoidx <btvoidx@gmail.com>
2020-12-17 20:56:47 +0400

    Added secret and logging (print :b:)

--- api/index.py
+++ api/index.py
@@ -1,13 +1,20 @@
 from sanic import Sanic
-from sanic.response import json, text
+from sanic.response import text
 from os import environ
 
 app = Sanic(name='api')
 
 @app.route('/api/callback', methods=['POST'])
 async def callback(request):
+	if environ.get('secret') != request.json.get('secret'):
+		return text('invalid secret')
+
 	if request.json.get('type') == 'confirmation':
 		return text(environ.get('confirmation_token', ''))
+	
+	print(request.json)
+
+	return text('ok')
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=8000, debug=True)
